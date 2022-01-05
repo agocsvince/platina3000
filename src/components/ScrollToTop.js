70e2ceba9a778798caae937a728743c2bdf4a65f
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2022-01-05 14:08:53 +0100

    implement ScrollToTop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -5,7 +5,6 @@ export default function ScrollToTop() {
   const location = useLocation();
 
   useEffect(() => {
-    console.log(location)
     if (location.pathname !== '/') {
       window.scrollTo(0, 0);
       console.log("scroll")
