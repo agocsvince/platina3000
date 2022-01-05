6108307a3d7c65208a103e192bb8a423b459d177
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2022-01-05 12:57:27 +0100

    implement handleClick function

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import logo from "../images/logo.png";
 import '../../style.css';
 import { Link } from 'react-router-dom';
@@ -6,20 +6,33 @@ import { HashLink } from 'react-router-hash-link';
 import ScriptTag from 'react-script-tag';
 
 const Header = props => {
+    const handleClick = useCallback(props => {
+        let pages = document.getElementsByClassName('pages')[0].childNodes;
+        pages.forEach(element => {
+            if (element !== null) {
+                if (element.id !== null) {
+                    element.id = "";
+                }
+            }
+        });
+
+        props.target.id = 'current-page'
+    }, [])
+
     return (
         <header className="flex">
                 <div className="logo">
                     <Link to="/"><img src={logo} alt="logo"/></Link>
                 </div>
                 <div className="pages mr-2">
-                    <HashLink to="/#house" id='current-page' >Főoldal</HashLink>
-                    <HashLink to="/#work" >Kivitelezés</HashLink>
-                    <HashLink to="/#prices" >Árak</HashLink>
-                    <HashLink to="/#planning" >Tervezés</HashLink>
-                    <HashLink to="/most-epul" >Most épül</HashLink>
-                    <Link to="/referenciak" >Referenciák</Link>
-                    <Link to="/elado" >Eladó</Link>
-                    <HashLink to="/#contact" >Kapcsolat</HashLink>
+                    <HashLink to="/#house" id='current-page' onClick={handleClick}>Főoldal</HashLink>
+                    <HashLink to="/#work" onClick={handleClick}>Kivitelezés</HashLink>
+                    <HashLink to="/#prices" onClick={handleClick}>Árak</HashLink>
+                    <HashLink to="/#planning" onClick={handleClick}>Tervezés</HashLink>
+                    <HashLink to="/most-epul" onClick={handleClick}>Most épül</HashLink>
+                    <Link to="/referenciak" onClick={handleClick}>Referenciák</Link>
+                    <Link to="/elado" onClick={handleClick}>Eladó</Link>
+                    <HashLink to="/#contact" onClick={handleClick}>Kapcsolat</HashLink>
                 </div>
                 <div className="hamburger">
                     <span className="bar"></span>
