7be8f129ae1979842b70b218e6553d232a3ec5d7
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2022-01-05 13:55:44 +0100

    implement MostEpuls.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Header from "./components/layouts/Header";
 import { ReferenceProvider } from './components/ReferenceContext';
 import { MostEpulProvider } from './components/MostEpulContext';
 import Main from './components/pages/Main';
+import Elado from './components/pages/Elado';
+import References from './components/pages/References';
+import MostEpuls from './components/pages/MostEpuls';
 
 
 const App = props => {
@@ -15,6 +18,9 @@ const App = props => {
       <MostEpulProvider>
       <Routes>
             <Route exact path='/' element={<Main />}/>
+            <Route path='/referenciak' element={<References />}/>
+            <Route path='/most-epul' element={<MostEpuls />}/>
+            <Route path='/elado' element={<Elado />}/>
         </Routes>
       </MostEpulProvider>
       </ReferenceProvider>
