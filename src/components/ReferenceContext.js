f32009a84a77328be1883bfc1d8b539637041e0e
vinceagocs <64377515+agocsvince@users.noreply.github.com>
2022-01-26 16:44:27 +0100

    remove image convertion

--- src/components/ReferenceContext.js
+++ src/components/ReferenceContext.js
@@ -12,7 +12,7 @@ const ReferenceProvider = (props) => {
 
     // Fetch data from CMS
     useEffect(() => {
-        const url = `https://api-eu-central-1.graphcms.com/v2/ckwq5z05y2e8n01xmgomm92ka/master?query=query%20MyQuery%20%7B%0A%20%20referenciaks%20%7B%0A%20%20%20%20gallery%20%7B%0A%20%20%20%20%20%20fileName%0A%20%20%20%20%20%20id%0A%20%20%20%20%20%20url(transformation%3A%20%7Bdocument%3A%20%7Boutput%3A%20%7Bformat%3A%20webp%7D%7D%7D)%0A%20%20%20%20%7D%0A%20%20%20%20title%0A%20%20%20%20id%0A%20%20%7D%0A%7D%0A`       
+        const url = `https://api-eu-central-1.graphcms.com/v2/ckwq5z05y2e8n01xmgomm92ka/master?query=query%20MyQuery%20%7B%0A%20%20referenciaks(orderBy%3A%20updatedAt_DESC)%20%7B%0A%20%20%20%20id%0A%20%20%20%20title%0A%20%20%20%20gallery%20%7B%0A%20%20%20%20%20%20fileName%0A%20%20%20%20%20%20url%0A%20%20%20%20%20%20id%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D&operationName=MyQuery`       
         fetch(url)
         .then(res => res.json())
         .then(json => {
